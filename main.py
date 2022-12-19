4fe9483d4f0c67b54682848c1d30c2fe56057067
Om Sarraf <itsomsarraf.github@gmail.com>
2022-12-20 01:25:25 +0530

    time was imported but not used

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import tkinter
 import customtkinter
 import sqlite3
-import time
-
 
 user_name = ""
 user_id = 0
-seller=False
+seller = False
 
-connection=sqlite3.connect("./database.db",isolation_level=None)
-cur= connection.cursor()
-customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+connection = sqlite3.connect("./database.db", isolation_level=None)
+cur = connection.cursor()
+# Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_appearance_mode("dark")
+# Themes: "blue" (standard), "green", "dark-blue"
+customtkinter.set_default_color_theme("blue")
 
 app = customtkinter.CTk()
 app.geometry("800x800")
@@ -21,22 +21,28 @@ app.title("PactumBU")
 frame_1 = customtkinter.CTkFrame(master=app)
 frame_1.pack(pady=20, padx=60, fill="both", expand=True)
 
-label_1 = customtkinter.CTkLabel(master=frame_1, text="PactumBU", justify=tkinter.CENTER, anchor=tkinter.CENTER, font=("Arial", 40, "bold"))
+label_1 = customtkinter.CTkLabel(master=frame_1, text="PactumBU",
+                                 justify=tkinter.CENTER, anchor=tkinter.CENTER, font=("Arial", 40, "bold"))
 label_1.pack(pady=10, padx=10, side="top", fill="both", expand=True)
 
-label_2 = customtkinter.CTkLabel(master=frame_1, text="Loading...", justify=tkinter.CENTER, anchor=tkinter.CENTER, font=("Arial", 20, "bold"))
+label_2 = customtkinter.CTkLabel(master=frame_1, text="Loading...",
+                                 justify=tkinter.CENTER, anchor=tkinter.CENTER, font=("Arial", 20, "bold"))
 label_2.pack(padx=10, side="top", fill="both", expand=True)
 frame_2 = customtkinter.CTkFrame(master=app)
 
 
-login_label = customtkinter.CTkLabel(master=frame_2, text="Login", justify=tkinter.CENTER, anchor=tkinter.CENTER, font=("Arial", 40, "bold"))
+login_label = customtkinter.CTkLabel(
+    master=frame_2, text="Login", justify=tkinter.CENTER, anchor=tkinter.CENTER, font=("Arial", 40, "bold"))
 login_label.pack(pady=10, padx=10, side="top", fill="both", expand=True)
 
-email = customtkinter.CTkEntry(master=frame_2, height=50, placeholder_text="Email")
+email = customtkinter.CTkEntry(
+    master=frame_2, height=50, placeholder_text="Email")
 email.pack(pady=10, padx=10, side="top", fill="x")
 
-password_validation = customtkinter.CTkEntry(master=frame_2, height=50, show="*", placeholder_text="Password")
+password_validation = customtkinter.CTkEntry(
+    master=frame_2, height=50, show="*", placeholder_text="Password")
 password_validation.pack(pady=10, padx=10, side="top", fill="x")
+
 
 def login():
     global user_name
@@ -44,7 +50,8 @@ def login():
     global seller
     user_email = email.get()
     password = password_validation.get()
-    cur.execute("SELECT * FROM account WHERE email = ? AND password = ?", (user_email, password))
+    cur.execute("SELECT * FROM account WHERE email = ? AND password = ?",
+                (user_email, password))
     data = cur.fetchall()
     response = customtkinter.CTkToplevel(app)
     response.geometry("400x400")
@@ -53,31 +60,35 @@ def login():
         user_id = data[0][0]
         if data[0][4] == 1:
             seller = True
-        res_label = customtkinter.CTkLabel(master=response, text="Login successful", justify=tkinter.CENTER, anchor=tkinter.CENTER, font=("Arial", 20, "bold"))
+        res_label = customtkinter.CTkLabel(master=response, text="Login successful",
+                                           justify=tkinter.CENTER, anchor=tkinter.CENTER, font=("Arial", 20, "bold"))
         res_label.pack(pady=10, padx=10, side="top", fill="both", expand=True)
+
         def proceed():
             global seller
             response.destroy()
             if seller:
                 import seller
-                s= seller.seller(app, user_name, user_id, cur)
+                s = seller.seller(app, user_name, user_id, cur)
                 s.create_frame(frame_2)
             else:
                 import buyer
                 b = buyer.buyer(app, user_name, user_id, cur)
-                b.create_frame(frame_2)              
+                b.create_frame(frame_2)
 
-        proceed_button = customtkinter.CTkButton(master=response, text="Proceed", command=proceed)
-        proceed_button.pack(pady=10, padx=10, side="top", expand=True)  
-
+        proceed_button = customtkinter.CTkButton(
+            master=response, text="Proceed", command=proceed)
+        proceed_button.pack(pady=10, padx=10, side="top", expand=True)
 
     else:
-        res_label = customtkinter.CTkLabel(master=response, text="Login Failed", justify=tkinter.CENTER, anchor=tkinter.CENTER, font=("Arial", 20, "bold"))
-        res_label.pack(pady=10, padx=10, side="top", fill="both", expand=True)        
+        res_label = customtkinter.CTkLabel(master=response, text="Login Failed",
+                                           justify=tkinter.CENTER, anchor=tkinter.CENTER, font=("Arial", 20, "bold"))
+        res_label.pack(pady=10, padx=10, side="top", fill="both", expand=True)
         res_label.after(3000, lambda: response.destroy())
-    
 
-login_button = customtkinter.CTkButton(master=frame_2, text="Login", command=login)
+
+login_button = customtkinter.CTkButton(
+    master=frame_2, text="Login", command=login)
 login_button.pack(pady=10, padx=10, side="top", expand=True)
 
 
@@ -86,6 +97,7 @@ def remove_splash():
     label_2.destroy()
     frame_1.destroy()
     frame_2.pack(pady=20, padx=60, fill="both", expand=True)
+
 
 label_1.after(2000, remove_splash)
 
